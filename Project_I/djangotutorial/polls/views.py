967e213afee6f51163476cb90c87cadb9d576315
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-17 19:33:57 +0300

    add shmindex view and URLconfig to polls

--- Project_I/djangotutorial/polls/views.py
+++ Project_I/djangotutorial/polls/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index.")
+
+
+def shmindex(request):
+    return HttpResponse("You are at polls shmindex.")
