8140b133f8cea7656e3d9dc438b9b77d5033cfac
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-19 21:02:24 +0300

    add urls for views detail(), results(), vote()

--- Project_I/djangotutorial/polls/urls.py
+++ Project_I/djangotutorial/polls/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("shm", views.shmindex, name="shmindex"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="resutls"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
