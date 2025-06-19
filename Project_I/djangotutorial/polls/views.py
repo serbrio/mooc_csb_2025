f1725bda1a25e6f3b2a52b8282040b447a79ded5
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-19 20:57:39 +0300

    add new views to polls: detail(), results(), vote()

--- Project_I/djangotutorial/polls/views.py
+++ Project_I/djangotutorial/polls/views.py
@@ -9,3 +9,17 @@ def index(request):
 
 def shmindex(request):
     return HttpResponse("You are at polls shmindex.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
+
