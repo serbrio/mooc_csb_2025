4ae01da94781e00203828da4c637d797eec2652e
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-20 19:16:27 +0300

    remove hardcoded urls from index.html template

--- Project_I/djangotutorial/polls/urls.py
+++ Project_I/djangotutorial/polls/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 
 urlpatterns = [
+    # the 'name' value as called by the  {% url %} template tag
     path("", views.index, name="index"),
     path("shm", views.shmindex, name="shmindex"),
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="resutls"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
