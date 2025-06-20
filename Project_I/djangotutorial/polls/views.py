69923ddc266901437d0b811c148d44de0acf9e0d
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-20 17:07:40 +0300

    add Http404 exception to detail() view; add detail.html template

--- Project_I/djangotutorial/polls/views.py
+++ Project_I/djangotutorial/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -17,7 +17,11 @@ def shmindex(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
