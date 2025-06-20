71039b60d34f64f2113dfea22920598d56c33be7
serbrio <28647145+serbrio@users.noreply.github.com>
2025-06-20 19:31:04 +0300

    add namespace to URLconf polls/urls.py

--- Project_I/djangotutorial/polls/urls.py
+++ Project_I/djangotutorial/polls/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 
+app_name = "polls"
 urlpatterns = [
     # the 'name' value as called by the  {% url %} template tag
     path("", views.index, name="index"),
     path("shm", views.shmindex, name="shmindex"),
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="resutls"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
