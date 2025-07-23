e2fb6e6d6d4cb8dd47aa41a0f3683b7faf73b40f
serbrio <28647145+serbrio@users.noreply.github.com>
2025-07-23 16:04:51 +0300

    cosmetic fixes in web_sms app

--- Project_I/djangotutorial/web_sms/models.py
+++ Project_I/djangotutorial/web_sms/models.py
@@ -12,7 +12,7 @@ class Account(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	balance = models.IntegerField()
 	secret_question = models.CharField(max_length=100)
-	secret_answer = models.CharField(max_length=40, validators=[validators.MinLengthValidator(5)]) # max_length=40 for sha1
+	secret_answer = models.CharField(max_length=40, validators=[validators.MinLengthValidator(5)])
 	## Fix Flaw 2
 	"""
 	@property
